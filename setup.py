afb110b7004cd1ffc0bbde977e31f5d6d383d825
Stefan Lehmann <Stefan.St.Lehmann@gmail.com>
2015-06-06 09:20:46 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='shopy',
+    version='0.0.1',
+    packages=['shopy'],
+    url='https://github.com/MrLeeh/shopy',
+    license='MIT',
+    author='Stefan Lehmann',
+    author_email='stefan.st.lehmann@gmail.com',
+    description=''
+)
