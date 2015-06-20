fc9aabf0007dbdcb9ee4bfa3c2286a47dbc87b4d
Stefan Lehmann <Stefan.St.Lehmann@gmail.com>
2015-06-20 13:42:45 +0200

    include test command in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,41 @@
 #!/usr/bin/env python
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['--strict', '--verbose', '--tb=long', 'tests']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 setup(
     name='shopy',
     version='0.0.1',
     packages=['shopy'],
+    package_data={'shopy': ['LICENSE', 'README.md']},
     url='https://github.com/MrLeeh/shopy',
-    license='MIT',
+    license='OSI Approved :: MIT License',
     author='Stefan Lehmann',
     author_email='stefan.st.lehmann@gmail.com',
-    description=''
+    description='',
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    platforms='any'
 )
